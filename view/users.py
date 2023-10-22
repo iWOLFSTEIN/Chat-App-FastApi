e5f2ed34b993550f68fc29b86cb90ad0a99e4715
Talha <talha@EAPPLEs-MacBook-Pro.local>
2023-10-22 22:35:36 +0500

    minor correction

--- view/users.py
+++ view/users.py
@@ -2,18 +2,23 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Query
 from controller.users import get_user_by_id, get_users
 from controller.validate import oauth2_scheme
+from utils.error_message import ErrorMessage
 
 router = APIRouter()
 
-@router.get('/users')
+
+@router.get("/users")
 async def users(
-    token: Annotated[str, Depends(oauth2_scheme)], 
-    limit: Annotated[int, Query()] = 20
-    ):
+    token: Annotated[str, Depends(oauth2_scheme)], limit: Annotated[int, Query()] = 20
+):
     return get_users(max_limit=limit, token=token)
 
-@router.get('/user')
+
+@router.get("/user")
 async def single_user(
-    token: Annotated[str, Depends(oauth2_scheme)],
-    id: Annotated[str, Query()]):
-    return get_user_by_id(id=id, token=token)
+    token: Annotated[str, Depends(oauth2_scheme)], id: Annotated[str, Query()]
+):
+    user = get_user_by_id(id=id, token=token)
+    if user:
+        return user
+    return {"message": ErrorMessage.user_not_found}
